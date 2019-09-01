1cb41216338a8cbc9990209c9ed381eac7a4cb37
TomasPabloFerreira <tomasferreira373@gmail.com>
2019-09-01 01:25:02 -0300

    Creación de menú y hoja de estilos

---
+++ web/menu.php
@@ -0,0 +1,17 @@
+<?php
+include_once 'header.php';
+?>
+
+<div class="col-lg-12">
+
+<ul class="list-group">
+	<li class="list-group-item gray-background white-color">Vistas</li>
+	<a href="private_grid.php" class="list-group-item list-group-item-action list-group-item-primary">Vista de Control</a>
+	<a href="public_grid.php" class="list-group-item list-group-item-action list-group-item-primary">Vista de cliente</a>
+</ul>
+</div>
+
+</div>
+</div>
+</body>
+</html>
